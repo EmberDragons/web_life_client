71d6b2daadca32bca0227a4e3a27c334dbfb934f
EmberDragons <118633501+EmberDragons@users.noreply.github.com>
2025-06-09 22:31:27 +0200

    update link

--- statics/script.js
+++ statics/script.js
@@ -2,7 +2,7 @@ import { FRAMERATE, GameLoop } from './js/GameLoop.js';
 import { WebSocket } from './js/WebSocket.js';
 import { CodiFiePassword } from './js/Password.js';
 
-export const SERVER_ADRESS = 'https://4f63-2a02-8428-37af-b01-f0ce-56d2-a083-8e59.ngrok-free.app';
+export const SERVER_ADRESS = 'https://bf50-2a02-8428-37af-b01-f0ce-56d2-a083-8e59.ngrok-free.app';
 
 var password_state = false;
 var max_server = 8;
